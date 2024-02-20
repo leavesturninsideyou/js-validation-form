102effd35d0e8fa3dfb9efa29bfe9344067b6419
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2024-02-20 14:38:57 -0800

    Remove showErrors + submit btn default event

--- src/index.js
+++ src/index.js
@@ -1,8 +1,52 @@
+import { confirmPwdError, mailError, pwdError, zipError } from "./inputErrors";
+
 const allInputs = document.querySelectorAll("input");
 const allSelects = document.querySelectorAll("select");
 
+const allSubmits = document.querySelectorAll(".submit");
+
 allInputs.forEach((input) => {
-  input.addEventListener("input", (event) => {});
+  input.addEventListener("input", (event) => {
+    const nearestErrorLabel = event.target.parentNode.querySelector(".error");
+
+    if (!input.validity.valid) {
+        if (input.id === "mail") {
+            mailError(input, nearestErrorLabel);
+        } else if (input.id === "zipcode") {
+            zipError(input, nearestErrorLabel);
+        }
+    } else if (input.id === "pwd") {
+        pwdError(input, nearestErrorLabel);
+    } else if (input.id === "confirm-pwd") {
+        confirmPwdError(input, nearestErrorLabel);
+    } else {
+        nearestErrorLabel.textContent = "";
+    }
+  });
 });
 
-allSelects.forEach((select) => {});
+allSelects.forEach(() => {
+
+});
+
+allSubmits.forEach((btn) => {
+    btn.addEventListener("click", (event) => {
+        allInputs.forEach((input) => {
+            if (!input.validity.valid) {
+                const nearestErrorLabel = input.parentNode.querySelector(".error");
+
+                if (input.id === "mail") {
+                    mailError(input, nearestErrorLabel);
+                } else if (input.id === "zipcode") {
+                    zipError(input, nearestErrorLabel);
+                } else if (input.id === "pwd") {
+                    pwdError(input, nearestErrorLabel);
+                }
+                // showErrors(input, "h", nearestErrorLabel);
+                event.preventDefault();
+            }
+        })
+    })
+});
+
+// submit button will do countryError
